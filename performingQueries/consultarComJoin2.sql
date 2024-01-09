7e8577869857e7996e1a23b2253461e985735d67
claralmoura <clara.moura@cotabox.com.br>
2024-01-09 19:42:25 -0300

    add(14): Realizando consulta com os tipos de JOIN

---
+++ performingQueries/consultarComJoin2.sql
@@ -0,0 +1,30 @@
+SELECT *
+FROM cidades c
+INNER JOIN prefeitos p
+ON c.id = p.cidade_id;
+
+SELECT *
+FROM cidades c
+LEFT JOIN prefeitos p
+ON c.id = p.cidade_id;
+
+SELECT *
+FROM cidades c
+LEFT OUTER JOIN prefeitos p
+ON c.id = p.cidade_id;
+
+SELECT *
+FROM cidades c
+RIGHT JOIN prefeitos p
+ON c.id = p.cidade_id;
+
+
+SELECT *
+FROM cidades c
+LEFT OUTER JOIN prefeitos p
+ON c.id = p.cidade_id
+UNION
+SELECT *
+FROM cidades c
+RIGHT JOIN prefeitos p
+ON c.id = p.cidade_id;
